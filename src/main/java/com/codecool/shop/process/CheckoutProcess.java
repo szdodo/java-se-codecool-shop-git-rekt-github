69f3696bf2171fc53f8470bed860f878dd7a5925
Péter Soltész <peter.soltesz92@gmail.com>
2017-04-27 16:01:23 +0200

    checkout page revamp; still needs some CSS

--- src/main/java/com/codecool/shop/process/CheckoutProcess.java
+++ src/main/java/com/codecool/shop/process/CheckoutProcess.java
@@ -1,10 +1,5 @@
 package com.codecool.shop.process;
 
 public class CheckoutProcess extends Process {
-    /*
-    @Override
-    protected void action (Orderable item) {
-        item.checkout();
-        System.out.println("Success! New status of the item with ID " + item.getId().toString() + " is: " + item.getStatus());
-    }*/
+
 }
