f676e3aad96c9bd27951878ef10acf7437b9c556
Szeghi Dorottya <szeghi.d@gmail.com>
2017-05-09 13:35:11 +0200

    db user added for simplicity

--- src/main/java/com/codecool/shop/dao/implementation/DBPassword.java
+++ src/main/java/com/codecool/shop/dao/implementation/DBPassword.java
@@ -1,10 +1,9 @@
 package com.codecool.shop.dao.implementation;
 
-/**
- * Created by robertgaspar on 09/05/2017.
- */
+
 public class DBPassword {
 
-    public static String password = "";
+    public static final String password = "";
+    public static final String user = "";
 
 }
