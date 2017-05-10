e03ff35eac456247d3464bc0a70ad24ce9a03dcb
Péter Soltész <peter.soltesz92@gmail.com>
2017-05-10 15:08:06 +0200

    added login page and CustomerControl

--- src/main/java/com/codecool/shop/controller/CustomerController.java
+++ src/main/java/com/codecool/shop/controller/CustomerController.java
@@ -1,22 +1,56 @@
 package com.codecool.shop.controller;
-import spark.ModelAndView;
-import java.util.HashMap;
+import com.codecool.shop.dao.implementation.DBConnection;
+
+import java.sql.*;
 
 
-public class CustomerController {
+public class CustomerController extends DBConnection {
 
-    public ModelAndView loginValidation(String username, String password) {
+    public Boolean loginValidation(String login, String pw) {
 
         // validate user bcrypt etc..
         // get customer from database
         // set session attribute: request.session().attribute("currentUser", customer);
         // redirect to home page
+        String query = "SELECT username, password FROM customer WHERE username = ? AND password = ? ";
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("asd", "dsa");
+        try {
+            Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, login);
+            statement.setString(2, pw);
+            ResultSet result = statement.executeQuery();
+            if (login.equals(result.getString("username")) && pw.equals(result.getString("password"))) {
+                return true;
+            }
 
-        return new ModelAndView(params, "product/index");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ez bizony false");
+        return false;
     }
 
+    public String getUserId(String username) {
+        String query = "SELECT name FROM customer WHERE username=" + username + ";";
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                System.out.println("sikerült! " + name);
+                return name;
+            }
+
+        } catch (
+                SQLException e)
+
+        {
+            e.printStackTrace();
+        }
+        return "getUserId failed";
+    }
 }
 
